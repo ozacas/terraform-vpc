4f1a6899bbcb871588f3647cb98de5c256994aaf
Andrew Cassin <acassin999@gmail.com>
2020-10-15 16:07:06 +1100

    Add RDS instance and associated infra

--- main.tf
+++ main.tf
@@ -50,15 +50,15 @@ resource "aws_route_table_association" "subnet1-public" {
     route_table_id = aws_route_table.subnet1-public.id
 }
 
-############################# private subnet
+############################# private subnet (in B AZ for db_subnet_group to have enough AZ coverage)
 resource "aws_subnet" "subnet2-private" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.99.124.0/24"
-  availability_zone = "ap-southeast-2a"
+  availability_zone = "ap-southeast-2b"
 }
 
 resource "aws_eip" "subnet2-nat" {
-  vpc   = true
+   vpc   = true
 }
 
 resource "aws_nat_gateway" "main" {
@@ -67,8 +67,8 @@ resource "aws_nat_gateway" "main" {
 }
 
 resource "aws_nat_gateway" "subnet2-private" {
-  allocation_id = aws_eip.subnet2-nat.id
-  subnet_id     = aws_subnet.subnet2-private.id
+   allocation_id = aws_eip.subnet2-nat.id
+   subnet_id     = aws_subnet.subnet2-private.id
 }
 
 resource "aws_route_table" "subnet2-private" {
@@ -93,21 +93,26 @@ resource "aws_route_table_association" "subnet2-private" {
 
 resource "aws_db_subnet_group" "postgresql" {
   name       = "main"
-  subnet_ids = [aws_subnet.subnet2-private.id]
+  subnet_ids = [aws_subnet.subnet2-private.id, aws_subnet.subnet1-public.id]
 
   tags = {
     Name = "Postgres subnet group"
   }
 }
 
+variable "rds_instance_size" {
+    type = string
+}
+
 resource "aws_db_instance" "postgresql" {
-    instance_class = var.db_instance_size
-    region = var.aws_region
+    instance_class = var.rds_instance_size
     allocated_storage    = 2
     storage_type         = "gp2"
     engine               = "postgres"
     name                 = "mydb"
-    username             = "admin"
-    password             = "admin"
-    db_subnet_group_name = aws.db_subnet_group.name
+    username             = "silly-admin" # ahhh... the good ole days
+    password             = "silly-admin"
+    db_subnet_group_name = aws_db_subnet_group.postgresql.name
+    availability_zone    = "ap-southeast-2a"
+    multi_az             = false
 }
